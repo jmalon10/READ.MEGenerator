4ebccc0971f9d8ff7dda82d6736d22d9dc4ce806
Jackie Maloney <jmaloney11277@gmail.com>
2024-09-18 23:35:31 -0400

    got inquirer prompts for each section heading, entered information needs to be accessed

--- Develop/index.js
+++ Develop/index.js
@@ -3,4 +3,35 @@ import inquirer from 'inquirer';
 //---------------------------DATA/STATE---------------------------------------------------------------------------//
 //---------------------------FUNCTIONS----------------------------------------------------------------------------//
 //---------------------------USER INTERACTIONS--------------------------------------------------------------------//
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'Please enter a Description of your application:',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Please enter Installation Instructions for your application:',
+      name: 'instructions',
+      },
+    {
+      type: 'input',
+      message: 'Please enter Usage Information for your application:',
+      name: 'usage',
+      }, 
+    {
+      type: 'input',
+      message: 'Please enter Contribution Guidelines for your application:',
+      name: 'guidelines',
+      },
+    {
+      type: 'input',
+      message: 'Please enter Test Instructions for your application:',
+      name: 'test',
+      },
+  ])
+  .then((response) =>
+    console.log('Success!')
+  );
 //---------------------------INITIALIZATOINS----------------------------------------------------------------------//
